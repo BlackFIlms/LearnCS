e541be31b368c12d534b2ec2f1c50504b6a945d0
BlackFilms <blackfilms13@gmail.com>
2019-02-07 17:43:39 +0500

    add project Lesson1(only System namespace) - SquareDigits 9119

---
+++ LearnCS/Lesson1(only System namespace) - SquareDigits 9119/Lesson1(only System namespace) - SquareDigits 9119.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson1_only_System_namespace____SquareDigits_9119
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console out.
+            Console.WriteLine(SquareDigits(9119));
+            Console.ReadKey();
+        }
+
+        public static int SquareDigits(int n)
+        {
+            string output = "";
+            foreach (char c in n.ToString())
+            {
+                int square = int.Parse(c.ToString());
+                output += (square * square);
+            }
+            return int.Parse(output);
+        }
+    }
+}
