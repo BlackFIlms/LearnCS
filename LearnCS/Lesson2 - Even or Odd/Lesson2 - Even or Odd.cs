dfe1705b2ed0dd875b99e0118f8f7e82b8c01d89
BlackFilms <blackfilms13@gmail.com>
2019-03-06 14:18:56 +0500

    New lesson. Lesson2 - Even or Odd

---
+++ LearnCS/Lesson2 - Even or Odd/Lesson2 - Even or Odd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson2___Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 478;
+            int reminder = number % 2;
+            string evenOdd = "";
+            if (reminder == 0)
+            {
+                evenOdd = "Even";
+            }
+            else
+            {
+                evenOdd = "Odd";
+            }
+            Console.WriteLine(reminder + "\n" + evenOdd);
+            Console.ReadKey();
+        }
+    }
+}
