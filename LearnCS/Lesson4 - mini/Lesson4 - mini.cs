fa91560ba6568cf8835faea0c25b045e741ebf75
BlackFilms <blackfilms13@gmail.com>
2019-03-07 12:45:16 +0500

    Add solution 'mini' for Lesson4.

---
+++ LearnCS/Lesson4 - mini/Lesson4 - mini.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputStr = "aaaxbbbbyyhwawiwjjjwwm";
+        string example = "abcdefghijklm";
+        int counter = 0;
+
+        foreach (char chIn in inputStr)
+        {
+            foreach (char chEx in example)
+            {
+                bool chCompare = chIn.Equals(chEx);
+                if (chCompare == true)
+                {
+                    counter++;
+                }
+            }
+        }
+        int errors = inputStr.Length - counter;
+        Console.WriteLine(errors + "/" + inputStr.Length);
+        Console.ReadKey();
+    }
+}
