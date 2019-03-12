e2eb58fe1da143f6c22173f2fe06e20aa3301b27
BlackFilms <blackfilms13@gmail.com>
2019-03-12 18:37:17 +0500

    Fix - Precision calc in Lesson6. Added new features in Lesson6.
    New features:
    Moved all calcs in functions.
    Divided one functions to some functions, for decrease code strings and increase code readability.
    For calc big numbers (bigger than e), used divide number on e, until the "number" is less than e.
    Added code execution time.
    Added debug strings to console.
    Debug strings colored to DarkGray.

--- LearnCS/Lesson6 - Log/Lesson6 - Log.cs
+++ LearnCS/Lesson6 - Log/Lesson6 - Log.cs
@@ -204,7 +204,7 @@ long count = 0;
         Console.ReadKey();*/
 
 /*
- * Therefore, we will calculate by the formula of an infinite series for the natural logarithm.
+ * Therefore, we will calculate by the formula of an infinite range for the natural logarithm.
  * Natural log - log(e)(z) = ln(z)
  * e = 2.71828182845905
  * ln(z)=ln((1+x)/(1-x))=2(x+(x^3/3)+(x^5/5)+...)
@@ -228,31 +228,32 @@ using System;
 
 class Program
 {
+
+    public const double e = 2.71828182845905;
+    public const double pi = 3.14159265358979;
+
     static void Main(string[] args)
     {
         string input = "";
         double log = 0;
         double logBase = 0;
-        double x = 0;
-        double x2 = 0;
-        double resultPow = 0;
-        double resultPow2 = 0;
 
+        //Get vars from user:
         Console.WriteLine("Input your number for log:");
         input = Console.ReadLine();
         if (input == "e")
         {
-            log = 2.71828182845905;
+            log = e;
         }
         else if (input == "pi")
         {
-            log = 3.14159265358979;
+            log = pi;
         }
         else
         {
             log = Double.Parse(input);
         }
-            
+
         Console.WriteLine("Input your base for log:");
         input = Console.ReadLine();
         if (input == "e")
@@ -263,38 +264,179 @@ class Program
         {
             logBase = 3.14159265358979;
         }
-        else
+        else if (String.IsNullOrEmpty(input) != true)
         {
             logBase = Double.Parse(input);
         }
 
-        //Find x:
-        x = (log - 1) / (1 + log);
+        //Define and startup debug timer.
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
 
-        //Use formula for calculate logarithm:
-        resultPow = 2 * (x + (Math.Pow(x, 3) / 3) + (Math.Pow(x, 5) / 5) + (Math.Pow(x, 7) / 7) + (Math.Pow(x, 9) / 9) + (Math.Pow(x, 11) / 11) + (Math.Pow(x, 13) / 13) + (Math.Pow(x, 15) / 15) + (Math.Pow(x, 17) / 17) + (Math.Pow(x, 19) / 19) + (Math.Pow(x, 21) / 21) + (Math.Pow(x, 23) / 23) + (Math.Pow(x, 25) / 25) + (Math.Pow(x, 27) / 27) + (Math.Pow(x, 29) / 29) + (Math.Pow(x, 31) / 31) + (Math.Pow(x, 33) / 33) + (Math.Pow(x, 35) / 35) + (Math.Pow(x, 37) / 37) + (Math.Pow(x, 39) / 39) + (Math.Pow(x, 41) / 41) + (Math.Pow(x, 43) / 43) + (Math.Pow(x, 45) / 45) + (Math.Pow(x, 47) / 47) + (Math.Pow(x, 49) / 49) + (Math.Pow(x, 51) / 51) + (Math.Pow(x, 53) / 53) + (Math.Pow(x, 55) / 55) + (Math.Pow(x, 57) / 57) + (Math.Pow(x, 59) / 59));
-        
-        Console.WriteLine("ln{0} = {1}", log, resultPow);
+        //Print result to console.
+        Console.WriteLine("ln{0} = {1}", log, MathLog(log));
         Console.WriteLine("Check this(c# method): ln{0} = {1}", log, Math.Log(log));
 
         if (logBase != 0)
         {
-            //Find x2:
-            x2 = (logBase - 1) / (1 + logBase);
-
-            //Use formula for calculate logarithm:
-            resultPow2 = 2 * (x2 + (Math.Pow(x2, 3) / 3) + (Math.Pow(x2, 5) / 5) + (Math.Pow(x2, 7) / 7) + (Math.Pow(x2, 9) / 9) + (Math.Pow(x2, 11) / 11) + (Math.Pow(x2, 13) / 13) + (Math.Pow(x2, 15) / 15) + (Math.Pow(x2, 17) / 17) + (Math.Pow(x2, 19) / 19) + (Math.Pow(x2, 21) / 21) + (Math.Pow(x2, 23) / 23) + (Math.Pow(x2, 25) / 25) + (Math.Pow(x2, 27) / 27) + (Math.Pow(x2, 29) / 29) + (Math.Pow(x2, 31) / 31) + (Math.Pow(x2, 33) / 33) + (Math.Pow(x2, 35) / 35) + (Math.Pow(x2, 37) / 37) + (Math.Pow(x2, 39) / 39) + (Math.Pow(x2, 41) / 41) + (Math.Pow(x2, 43) / 43) + (Math.Pow(x2, 45) / 45) + (Math.Pow(x2, 47) / 47) + (Math.Pow(x2, 49) / 49) + (Math.Pow(x2, 51) / 51) + (Math.Pow(x2, 53) / 53) + (Math.Pow(x2, 55) / 55) + (Math.Pow(x2, 57) / 57) + (Math.Pow(x2, 59) / 59));
-
-            Console.WriteLine("ln{0} = {1}", logBase, resultPow2);
+            Console.WriteLine("ln{0} = {1}", logBase, MathLog(logBase));
             Console.WriteLine("Check this(c# method): ln{0} = {1}", logBase, Math.Log(logBase));
-            Console.WriteLine("log({0}){1} = {2}", logBase, log, resultPow/resultPow2);
-            Console.WriteLine("Check this(c# method): log({0}){1} = {2}", logBase, log, Math.Log(log,logBase));
+            Console.WriteLine("log({0}){1} = {2}", logBase, log, MathLog(log, logBase));
+            Console.WriteLine("Check this(c# method): log({0}){1} = {2}", logBase, log, Math.Log(log, logBase));
+        }
+
+        //Stop debug timer and print it to console.
+        sw.Stop();
+        Console.WriteLine("Code execution time: {0}ms", (sw.ElapsedMilliseconds / 100.0).ToString());
+
+        Console.ReadKey();
+    }
+
+    //Determines how large a log is. For math log to base e.
+    public static double MathLog(double log)
+    {
+        double pow = 0;
+
+        if (log <= e)
+        {
+            pow = MathLogSingle(log);
+        }
+        else if (log > e)
+        {
+            int count = 0;
+            while (log > e)
+            {
+                log /= e;
+                count++;
+            }
+            pow = count + MathLogSingle(log);
+        }
+
+        return pow;
+    }
+
+    //Determines how large a log is. For math log to other base.
+    public static double MathLog(double log, double logBase)
+    {
+        double pow = 0;
+
+        if (log <= e  && logBase <= e)
+        {
+            pow = MathLogSingle(log) / MathLogSingle(logBase);
         }
         else
         {
-            Console.WriteLine("ln0 = 0");
+            pow = MathLog(log) / MathLog(logBase);
         }
 
-        Console.ReadKey();
+        return pow;
+    }
+
+    //Log calc function.
+    public static double MathLogSingle(double log)
+    {
+        double x = 0;
+        double pow = 0;
+        int rangeValue = 40;
+        double percentage = rangeValue / 100d;
+
+        if (log > 0 && log != 1)
+        {
+            //Find x:
+            x = (log - 1) / (1 + log);
+
+            //Use formula for calculate logarithm:
+            for (int i = 1; i < rangeValue; i += 2)
+            {
+                pow += 2 * ((Math.Pow(x, i) / i));
+
+                //Debug info.
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Progress: {0} of {1}. In percents: {2} of 100%", i, rangeValue, Math.Round(i / percentage, 1) );
+                if (i < rangeValue - 1)
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    ClearCurrentConsoleLine();
+                }
+                Console.ResetColor();
+            }
+
+            //Use formula for calculate logarithm:
+            //pow = 2 * (x + (Math.Pow(x, 3) / 3) + (Math.Pow(x, 5) / 5) + (Math.Pow(x, 7) / 7) + (Math.Pow(x, 9) / 9) + (Math.Pow(x, 11) / 11) + (Math.Pow(x, 13) / 13) + (Math.Pow(x, 15) / 15) + (Math.Pow(x, 17) / 17) + (Math.Pow(x, 19) / 19) + (Math.Pow(x, 21) / 21) + (Math.Pow(x, 23) / 23) + (Math.Pow(x, 25) / 25) + (Math.Pow(x, 27) / 27) + (Math.Pow(x, 29) / 29) + (Math.Pow(x, 31) / 31) + (Math.Pow(x, 33) / 33) + (Math.Pow(x, 35) / 35) + (Math.Pow(x, 37) / 37) + (Math.Pow(x, 39) / 39) + (Math.Pow(x, 41) / 41) + (Math.Pow(x, 43) / 43) + (Math.Pow(x, 45) / 45) + (Math.Pow(x, 47) / 47) + (Math.Pow(x, 49) / 49) + (Math.Pow(x, 51) / 51) + (Math.Pow(x, 53) / 53) + (Math.Pow(x, 55) / 55) + (Math.Pow(x, 57) / 57) + (Math.Pow(x, 59) / 59));
+        }
+
+        return pow;
+    }
+
+    //It's copypaste coding, don't cool =) 
+    /*public static double MathLog(double log_, double logBase_, out double pow2)
+    {
+        double x = 0;
+        double x2 = 0;
+        double pow = 0;
+        pow2 = 0;
+        const int rangeValue = 80;
+        double percentage = rangeValue / 100d;
+
+        if (log_ > 0 && log_ != 1)
+        {
+            //Find x:
+            x = (log_ - 1) / (1 + log_);
+
+            //Use formula for calculate logarithm:
+            for (int i = 1; i < rangeValue; i += 2)
+            {
+                pow += 2 * ((Math.Pow(x, i) / i));
+
+                //Debug info.
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Progress: {0} of {1}. In percents: {2} of 100%", i, rangeValue, Math.Round(i / percentage, 1));
+                if (i < rangeValue - 1)
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    ClearCurrentConsoleLine();
+                }
+                Console.ResetColor();
+            }
+
+            //Use formula for calculate logarithm:
+            //pow = 2 * (x + (Math.Pow(x, 3) / 3) + (Math.Pow(x, 5) / 5) + (Math.Pow(x, 7) / 7) + (Math.Pow(x, 9) / 9) + (Math.Pow(x, 11) / 11) + (Math.Pow(x, 13) / 13) + (Math.Pow(x, 15) / 15) + (Math.Pow(x, 17) / 17) + (Math.Pow(x, 19) / 19) + (Math.Pow(x, 21) / 21) + (Math.Pow(x, 23) / 23) + (Math.Pow(x, 25) / 25) + (Math.Pow(x, 27) / 27) + (Math.Pow(x, 29) / 29) + (Math.Pow(x, 31) / 31) + (Math.Pow(x, 33) / 33) + (Math.Pow(x, 35) / 35) + (Math.Pow(x, 37) / 37) + (Math.Pow(x, 39) / 39) + (Math.Pow(x, 41) / 41) + (Math.Pow(x, 43) / 43) + (Math.Pow(x, 45) / 45) + (Math.Pow(x, 47) / 47) + (Math.Pow(x, 49) / 49) + (Math.Pow(x, 51) / 51) + (Math.Pow(x, 53) / 53) + (Math.Pow(x, 55) / 55) + (Math.Pow(x, 57) / 57) + (Math.Pow(x, 59) / 59));
+        }
+        
+        if (logBase_ > 0 && logBase_ != 1)
+        {
+            //Find x2:
+            x2 = (logBase_ - 1) / (1 + logBase_);
+
+            //Use formula for calculate logarithm:
+            for (int i = 1; i < rangeValue; i += 2)
+            {
+                pow2 += 2 * ((Math.Pow(x2, i) / i));
+
+                //Debug info.
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Progress: {0} of {1}. In percents: {2} of 100%", i, rangeValue, Math.Round(i / percentage, 1));
+                if (i < rangeValue - 1)
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    ClearCurrentConsoleLine();
+                }
+                Console.ResetColor();
+            }
+
+            //Use formula for calculate logarithm:
+            //pow2 = 2 * (x2 + (Math.Pow(x2, 3) / 3) + (Math.Pow(x2, 5) / 5) + (Math.Pow(x2, 7) / 7) + (Math.Pow(x2, 9) / 9) + (Math.Pow(x2, 11) / 11) + (Math.Pow(x2, 13) / 13) + (Math.Pow(x2, 15) / 15) + (Math.Pow(x2, 17) / 17) + (Math.Pow(x2, 19) / 19) + (Math.Pow(x2, 21) / 21) + (Math.Pow(x2, 23) / 23) + (Math.Pow(x2, 25) / 25) + (Math.Pow(x2, 27) / 27) + (Math.Pow(x2, 29) / 29) + (Math.Pow(x2, 31) / 31) + (Math.Pow(x2, 33) / 33) + (Math.Pow(x2, 35) / 35) + (Math.Pow(x2, 37) / 37) + (Math.Pow(x2, 39) / 39) + (Math.Pow(x2, 41) / 41) + (Math.Pow(x2, 43) / 43) + (Math.Pow(x2, 45) / 45) + (Math.Pow(x2, 47) / 47) + (Math.Pow(x2, 49) / 49) + (Math.Pow(x2, 51) / 51) + (Math.Pow(x2, 53) / 53) + (Math.Pow(x2, 55) / 55) + (Math.Pow(x2, 57) / 57) + (Math.Pow(x2, 59) / 59));
+        }
+
+        return pow;
+    }*/
+
+    //Clear this line in console.
+    public static void ClearCurrentConsoleLine()
+    {
+        int currentLineCursor = Console.CursorTop;
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write(new string(' ', Console.WindowWidth));
+        Console.SetCursorPosition(0, currentLineCursor);
     }
 }
--- LearnCS/test/test.cs
+++ LearnCS/test/test.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace test
@@ -13,12 +14,32 @@ namespace test
             double num = 2.7;
             double baseNum = 2;
             double x = Math.Log(num,baseNum);
+            double percentage = 30 / 100d;
 
+            Console.WriteLine("Main thread: starting a timer");
+            Timer t = new Timer(ComputeBoundOp, 1, 0, 1000);
+            Console.WriteLine("Main thread: Doing other work here...");
+            Thread.Sleep(3000); // Simulating other work (10 seconds)
+            t.Dispose(); // Cancel the timer now
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Percentage 30 of 100: " + percentage);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(2%1);
             Console.WriteLine(x);
             Console.WriteLine(Math.Log(2.71828182845905));
             Console.WriteLine(Math.E);
             Console.WriteLine(0 % 10);
             Console.ReadKey();
+        }
+
+        private static void ComputeBoundOp(Object state)
+        {
+            // This method is executed by a thread pool thread 
+            Console.WriteLine("In ComputeBoundOp: state={0}", state);
+            Thread.Sleep(1000); // Simulates other work (1 second)
+                                // When this method returns, the thread goes back 
+                                // to the pool and waits for another task 
         }
     }
 }
