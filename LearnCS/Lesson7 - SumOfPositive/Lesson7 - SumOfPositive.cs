5cabc90d4df40527ee4ec18810fa906d0455b34d
BlackFilms <blackfilms13@gmail.com>
2019-04-04 01:01:07 +0500

    Finished writing Lesson8 - GoodVsEvil with OOP. Same fix all problems. Same add restart Program. All works good!

--- LearnCS/Lesson7 - SumOfPositive/Lesson7 - SumOfPositive.cs
+++ LearnCS/Lesson7 - SumOfPositive/Lesson7 - SumOfPositive.cs
@@ -1,4 +1,5 @@
-﻿/*
+﻿//Task
+/*
 You get an array of numbers, return the sum of all of the positives ones.
 
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
--- LearnCS/Lesson8 - GoodVsEvil/Lesson8 - GoodVsEvil.cs
+++ LearnCS/Lesson8 - GoodVsEvil/Lesson8 - GoodVsEvil.cs
@@ -1,57 +1,159 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//Task
+/*Description
+
+Middle Earth is about to go to war. The forces of good will have many battles with the forces of evil. Different races will certainly be involved. Each race has a certain worth when battling against others. On the side of good we have the following races, with their associated worth:
+
+    Hobbits: 1
+    Men: 2
+    Elves: 3
+    Dwarves: 3
+    Eagles: 4
+    Wizards: 10
+
+On the side of evil we have:
+
+    Orcs: 1
+    Men: 2
+    Wargs: 2
+    Goblins: 2
+    Uruk Hai: 3
+    Trolls: 5
+    Wizards: 10
+
+Although weather, location, supplies and valor play a part in any battle, if you add up the worth of the side of good and compare it with the worth of the side of evil, the side with the larger worth will tend to win.
+
+Thus, given the count of each of the races on the side of good, followed by the count of each of the races on the side of evil, determine which side wins.
+Input:
+
+The function will be given two parameters. Each parameter will be a string separated by a single space. Each string will contain the count of each race on the side of good and evil.
+
+The first parameter will contain the count of each race on the side of good in the following order:
+
+    Hobbits, Men, Elves, Dwarves, Eagles, Wizards.
+
+The second parameter will contain the count of each race on the side of evil in the following order:
+
+    Orcs, Men, Wargs, Goblins, Uruk Hai, Trolls, Wizards.
+
+All values are non-negative integers. The resulting sum of the worth for each side will not exceed the limit of a 32-bit integer.
+Output:
+
+Return "Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, or "Battle Result: No victor on this battle field" if it ends in a tie.
+*/
+
+using System;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //Print Values of each side.
-        Console.WriteLine("Forces of the good side:");
-        Console.WriteLine(goodSideForces);
-        Console.WriteLine("Forces of the evil side:");
-        Console.WriteLine(evilSideForces);
-        foreach (var item in Parse.Parser(goodSideForces))
+        char control = 'z';
+        while (true)
         {
-            Console.WriteLine(item);
-        }
-        foreach (var item in Parse.Parser(evilSideForces))
-        {
-            Console.WriteLine(item);
+            //Create each side.
+            Side Good = new Side();
+            Side Evil = new Side();
+
+            //Set worth and get and set force for each side.
+            Good.ForcesCount = 6;
+            Good.Worth = "1 2 3 3 4 10";
+            Good.Forces = Logic.RandCount(Good.ForcesCount);
+            Evil.ForcesCount = 7;
+            Evil.Worth = "1 2 2 2 3 5 10";
+            Evil.Forces = Logic.RandCount(Evil.ForcesCount);
+
+            //Print all variables for each side.
+            Console.WriteLine(Good.Worth);
+            Console.WriteLine(Good.Forces);
+            Console.WriteLine();
+            Console.WriteLine(Evil.Worth);
+            Console.WriteLine(Evil.Forces);
+            Console.WriteLine();
+
+            //Get and print battle result.
+            Console.WriteLine(Logic.BattleResult(Good, Evil, out int goodPower, out int evilPower));
+            Console.WriteLine("Sides power:" + "\r\n" + "Good - {0}" + "\r\n" + "Evil - {1}", goodPower, evilPower);
+
+            control = ConsoleRestart();
+
+            if (control == 'x')
+            {
+                break;
+            }
         }
 
         Console.ReadKey();
     }
-}
 
-class Good
-{
-    private string goodSideForces = Rand.RandCount(6);
-    public string GoodSideForces
+    private static char ConsoleRestart()
     {
-        get { return goodSideForces; }
+        Char xKey = 'z';
+        while (xKey != 'r' || xKey != 'x')
+        {
+            xKey = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Console.WriteLine(xKey);
+            if (xKey == 'r')
+            {
+                return xKey;
+            }
+            else if (xKey == 'x')
+            {
+                return xKey;
+            }
+        }
+        return 'x';
     }
 }
 
-class Evil
+class Side
 {
-    private string evilSideForces = Rand.RandCount(7);
-    public string EvilSideForces
+    //Set force and worth values of side.
+    private int forcesCount = 0;
+    private string worth = "";
+    private string forces = "";
+
+    //Create private array for all values.
+    private int[] worthParsed;
+    private int[] forcesParsed;
+
+    //Let's make it possible to get the values of these variables from outside.
+    public int ForcesCount
     {
-        get { return evilSideForces; }
+        get { return forcesCount; }
+        set { forcesCount = value; }
+    }
+    public string Worth
+    {
+        get { return worth; }
+        set { worth = value; }
+    }
+    public string Forces
+    {
+        get { return forces; }
+        set { forces = value; }
+    }
+    public int[] WorthParsed
+    {
+        get { return worthParsed; }
+        set { worthParsed = value; }
+    }
+    public int[] ForcesParsed
+    {
+        get { return forcesParsed; }
+        set { forcesParsed = value; }
     }
 }
 
-static class Rand
+static class Logic
+//Contains all functions for calculate battle result.
 {
     //If create object in function RandCount, then this create new object  instance every time, when this function has been called.
     //And function Random, create similar values.
     static Random rand = new Random();
 
-    public static string RandCount(int count)
+    public static string RandCount(int count) //Makes a string with any number of numeric values separated by spaces.
     {
         string res = "";
         for (int i = 0; i < count; i++)
@@ -63,15 +165,13 @@ static class Rand
         }
         return res;
     }
-}
 
-static class Parse
-{
-    public static Array Parser(string z)
+
+    public static Array Parser(string z) //Parses strings with numbers separated by spaces. If it does, then inserts the values into the array.
     {
         string pattern = @"(\d+)\s?";
         MatchCollection matches = Regex.Matches(z, pattern); //Scan input string with pattern.
-        
+
         if (matches.Count > 0)
         {
             int[] res = new int[matches.Count];
@@ -87,6 +187,117 @@ static class Parse
         {
             int[] res = { 0 };
             return res;
+        }
+    }
+
+    private static void DefineArrays(Side obj)
+    {
+        //Define Arrays.
+        obj.WorthParsed = new int[obj.ForcesCount];
+        obj.ForcesParsed = new int[obj.ForcesCount];
+
+        //Put all values in private defined arrays.
+        int j = 0;
+        foreach (int item in Parser(obj.Worth))
+        {
+            obj.WorthParsed[j] = item;
+            j++;
+        }
+        int k = 0;
+        foreach (int item in Parser(obj.Forces))
+        {
+            obj.ForcesParsed[k] = item;
+            k++;
+        }
+    }
+
+    public static Array AssocWorthAndForce(Side obj)//Create 2 dimensions array and insert values of side there.
+    {
+        DefineArrays(obj);
+        int[,] assocWorthAndForce = new int[obj.ForcesParsed.Length, obj.WorthParsed.Length];
+        for (int i = 0; i < obj.WorthParsed.Length; i++)
+        {
+            assocWorthAndForce[i, 1] = Convert.ToInt32(obj.WorthParsed.GetValue(i));
+        }
+        for (int i = 0; i < obj.ForcesParsed.Length; i++)
+        {
+            assocWorthAndForce[i, 2] = Convert.ToInt32(obj.ForcesParsed.GetValue(i));
+        }
+
+        return assocWorthAndForce;
+    }
+
+    public static string BattleResult(Side Good, Side Evil)
+    {
+        //Get power for Good side.
+        int goodPower = 0;
+        for (int i = 0; i < AssocWorthAndForce(Good).GetLength(1); i++)
+        {
+            for (int j = 0; j < AssocWorthAndForce(Good).GetLength(2); j++)
+            {
+                goodPower += Convert.ToInt32(AssocWorthAndForce(Good).GetValue(i)) * Convert.ToInt32(AssocWorthAndForce(Good).GetValue(j));
+            }
+        }
+
+        //Get power for Evil side.
+        int evilPower = 0;
+        for (int i = 0; i < AssocWorthAndForce(Evil).GetLength(1); i++)
+        {
+            for (int j = 0; j < AssocWorthAndForce(Evil).GetLength(2); j++)
+            {
+                evilPower += Convert.ToInt32(AssocWorthAndForce(Evil).GetValue(i)) * Convert.ToInt32(AssocWorthAndForce(Evil).GetValue(j));
+            }
+        }
+
+        //Compare powers and return result.
+        if (goodPower > evilPower)
+        {
+            return "Battle Result: Good triumphs over Evil";
+        }
+        else if (goodPower < evilPower)
+        {
+            return "Battle Result: Evil eradicates all trace of Good";
+        }
+        else
+        {
+            return "Battle Result: No victor on this battle field";
+        }
+    }
+
+    public static string BattleResult(Side Good, Side Evil, out int goodPower, out int evilPower)
+    {
+        //Get power for Good side.
+        goodPower = 0;
+        for (int i = 0; i < AssocWorthAndForce(Good).GetLength(0); i++)
+        {
+            for (int j = 0; j < AssocWorthAndForce(Good).GetLength(1); j++)
+            {
+                goodPower += Convert.ToInt32(AssocWorthAndForce(Good).GetValue(0,i)) * Convert.ToInt32(AssocWorthAndForce(Good).GetValue(1,j));
+            }
+        }
+
+        //Get power for Evil side.
+        evilPower = 0;
+        for (int i = 0; i < AssocWorthAndForce(Evil).GetLength(0); i++)
+        {
+            for (int j = 0; j < AssocWorthAndForce(Evil).GetLength(1); j++)
+            {
+                evilPower += Convert.ToInt32(AssocWorthAndForce(Evil).GetValue(0,i)) * Convert.ToInt32(AssocWorthAndForce(Evil).GetValue(1,j));
+            }
+        }
+
+        //Compare powers and return result.
+        if (goodPower > evilPower)
+        {
+            return "Battle Result: Good triumphs over Evil";
+        }
+        else if (goodPower < evilPower)
+        {
+            return "Battle Result: Evil eradicates all trace of Good";
+        }
+        else
+        {
+            return "Battle Result: No victor on this battle field";
         }
     }
 }
